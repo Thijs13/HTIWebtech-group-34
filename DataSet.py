a0e038e62b6e8a54af9a3d4f6a96ccd311c7f03b
Thijs13 <thijs98joosten@gmail.com>
2019-05-06 22:39:27 +0200

    Added the DataSet class as an additional way to store the data

---
+++ DataSet.py
@@ -0,0 +1,23 @@
+class DataSet:
+
+    def __init__(self, nodes):
+        self.nodes = nodes
+
+    def getNodes(self):
+        return self.nodes
+
+    def setNodes(self, nodes):
+        self.nodes = nodes
+
+    def addNote(self, node):
+        self.nodes.append(node)
+
+    def getNode(self, name):
+        for node in self.nodes:
+            if node.name == name:
+                return node
+
+# needs testing
+    def deleteNode(self, target):
+        self.nodes.remove(target)
+
