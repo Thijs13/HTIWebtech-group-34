6a2f54730ee3216daf9cb47e2cef0a986344ebb3
JeroenHageman <jeroen7991@live.nl>
2019-05-09 15:54:01 +0200

    Adjacency matrix

---
+++ AdjacencyMatrix.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+# sphinx_gallery_thumbnail_number = 2
+
+
+class AdjacencyMatrix:
+    def makeMatrix(self, ds):
+        names = []
+        nodes = []
+        for i in ds.getNodes():
+            names.append(i.getName())
+        for i in range(len(ds.getNodes())):
+            nodes.append([])
+            print(i)
+            for j in range(len(ds.getNodes())):
+                nodes[i].append(ds.getNodes()[i].getLinks()[j][1])
+                print(j)
+
+        y = names
+        x = names
+
+        nodes = np.array(nodes)
+
+
+        fig, ax = plt.subplots()
+        im = ax.imshow(nodes)
+
+        # We want to show all ticks...
+        ax.set_xticks(np.arange(len(x)))
+        ax.set_yticks(np.arange(len(y)))
+        # ... and label them with the respective list entries
+        ax.set_xticklabels(x)
+        ax.set_yticklabels(y)
+
+        # Rotate the tick labels and set their alignment.
+        plt.setp(ax.get_xticklabels(), rotation=145, ha="right",
+                 rotation_mode="anchor")
+
+        # Loop over data dimensions and create text annotations.
+        for i in range(len(y)):
+            for j in range(len(x)):
+                text = ax.text(j, i, nodes[i, j],
+                               ha="center", va="center", color="w")
+
+        ax.set_title("Zellufgemaakt")
+        fig.tight_layout()
+        plt.show()
