bbee320bbb261adcfa83aa36250380835a51a85a
s164550 <j.hageman@student.tue.nl>
2019-06-12 13:07:35 +0200

    Bokeh Matrix interactions

--- AdjacencyMatrix.py
+++ AdjacencyMatrix.py
@@ -7,12 +7,15 @@ from bokeh.io import output_file, show
 from bokeh.models import BasicTicker, ColorBar, LinearColorMapper, ColumnDataSource, PrintfTickFormatter
 from bokeh.plotting import figure
 from bokeh.transform import transform
+import webcolors as wc
 
 
 class AdjacencyMatrix:
     def makeMatrix(self, ds):
+        # getting data from FileLoader and creating the right format
         names = []
         # nodes = []
+
         for i in ds.getNodes():
             names.append(i.getName())
         nodes = ds.getDoubleList(0, True)
@@ -31,30 +34,35 @@ class AdjacencyMatrix:
         df = df.stack().rename("value").reset_index()
 
 
-        # here the plot :
+        # Making the plot html file
         output_file("matrixPlot.html")
 
-        # You can use your own palette here
+        # Creating the array containing the colors
         colorList = []
-        for i in range(100):
-            color = (255, 255, i)
+        i = 0
+        while i < 256:
+            color = wc.rgb_to_hex((255-(i-10), 255-(i-20), 255-(i-30)))
             colorList.append(color)
+            i = i + 51
         colors = colorList
-        #[(255, 255, 255), '#fc942f', '#fda148', '#fdae61', '#fdbb7a', '#fec893', '#fed5ad']
-        colors.reverse()
-        # Had a specific mapper to map color with value
+
+        # This part maps the colors at intervals
         mapper = LinearColorMapper(
             palette=colors, low=df.value.min(), high=df.value.max())
-        # Define a figure
+
+        # Creating the figure
         p = figure(
             plot_width=800,
             plot_height=800,
-            title="Matrix Visualization",
+            title="Adjacency Matrix",
             x_range=list(df.X.drop_duplicates()),
             y_range=list(df.Y.drop_duplicates()),
-            toolbar_location="below",
-            tools="pan,wheel_zoom,box_zoom,reset",
+
+            # Adding a toolbar
+            toolbar_location="right",
+            tools="hover,pan,box_zoom,reset",
             x_axis_location="above")
+
         # Create rectangle for heatmap
         p.rect(
             x="X",
@@ -64,12 +72,21 @@ class AdjacencyMatrix:
             source=ColumnDataSource(df),
             line_color=None,
             fill_color=transform('value', mapper))
+
         # Add legend
         color_bar = ColorBar(
             color_mapper=mapper,
             location=(0, 0),
             ticker=BasicTicker(desired_num_ticks=len(colors)))
 
+        if len(list(df.X.drop_duplicates())) > p.plot_width / 4:
+            p.xaxis.visible = False
+        else:
+            pass
+        if len(list(df.Y.drop_duplicates())) > p.plot_height / 4:
+            p.yaxis.visible = False
+        else:
+            pass
         p.xaxis.major_label_orientation = 'vertical'
         p.add_layout(color_bar, 'right')
         show(p)
