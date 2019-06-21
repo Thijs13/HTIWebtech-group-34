c502fd0f85547d7c45a3dbe3a49ee3dceabefc4f
s164550 <j.hageman@student.tue.nl>
2019-06-21 12:19:55 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ptviz',
+    version='2.3',
+    packages=[''],
+    url='',
+    license='',
+    author='s165319',
+    author_email='',
+    description=''
+)
